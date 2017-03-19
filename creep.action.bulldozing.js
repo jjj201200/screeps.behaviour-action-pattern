a9cc85c9257125b91f173badccede469567047ee
Spedwards <improbablepuzzle@gmail.com>
2017-03-20 04:08:36 +1100

    travelTo to move

--- creep.action.bulldozing.js
+++ creep.action.bulldozing.js
@@ -22,7 +22,7 @@ action.newTarget = function(creep) {
         });
 };
 action.work = function(creep) {
-    return creep.travelTo(creep.target);
+    return creep.move(creep.pos.getDirectionTo(creep.target));
 };
 action.onAssignment = function(creep) {
     if (SAY_ASSIGNMENT) creep.say(String.fromCodePoint(0x1F4CC), SAY_PUBLIC);
